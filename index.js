eff9cf76d6dec7bf1def443bbb044171f85fb57f
Ather1230 <ather.biswas0057@gmail.com>
2022-10-27 04:07:30 +0600

    course data loaded

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 app.use(cors());
 
-const courses = require('./data/course.json')
+const courses = require('./data/course.json');
+const course = require('./data/course.json');
 
 app.get('/', (req, res) => {
     res.send('course api running');
@@ -12,6 +13,13 @@ app.get('/', (req, res) => {
 
 app.get('/courses', (req, res) => {
     res.send(courses);
+});
+
+app.get('/courses/:id', (req, res) =>{
+    console.log(req.params.id)
+    const id = req.params.id;
+    const selectedCourse = courses.find( c => c._id === id);
+    res.send(selectedCourse);
 })
 
 app.listen(port, () => {
